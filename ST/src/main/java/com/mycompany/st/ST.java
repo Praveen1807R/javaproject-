52b92053acc288069a6bed2244ad2c822328be88
Praveen1807R <155814479+Praveen1807R@users.noreply.github.com>
2024-11-26 20:41:09 +0530

    Add files via upload

---
+++ ST/src/main/java/com/mycompany/st/ST.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.st;
+
+/**
+ *
+ * @author prave
+ */
+public class ST {
+
+    public static void main(String[] args) {
+        
+        mii m=new mii();
+        m.setVisible(true);
+    }
+}
